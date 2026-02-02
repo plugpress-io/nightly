75d11b9371c5a323dda049195b6f703416e4f3bd
fahimreza-dev <fahim@plugpress.co>
2026-02-02 17:32:26 +0600

    Initialize

--- build/admin/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c4b94c9fc0a4f7c90ada');
--- build/frontend/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '3ccdfed09e9be57d0428');
